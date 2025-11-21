7b208f0e7ae13930412073a6039a12571fa6ab79
stevec-afk <evets25@gmail.com>
2025-11-21 15:26:34 -0400

    refactored javascript, basic operations

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const expressionDiv = document.getElementById('expression');
 const resultDiv = document.getElementById('result');
 
 let expression = '';
-let result = '';
+let result = 0;
+let prevOperand = '';
+let currOperand = '';
+let operator = '';
+let pressedSubmit = false;
 
 keypad.addEventListener('click', buttonClick);
 
@@ -12,80 +16,122 @@ function buttonClick(event) {
     const action = target.dataset.action;
     const value = target.dataset.value;
     // console.log(target, action, value);
+
     switch (action) {
         case 'clear':
             clear();
+            console.clear();
             break;
         case 'neg':
-            negate();
+            if (expression == '' || expression == 0){
+                return;
+            }
+            expression *= -1;
             break;
         case 'mod':
-            percentage();
+            expression = expression / 100;
             break;
         case 'decimal':
-            decimal(value);
+            decimal();
             break
-        case 'number':
-            appendValue(value);
-            break;
         case 'backspace':
-            backspace();
+            expression = expression.slice(0, -1);
             break;
         case 'submit':
-            evaluateExpression();
+            if (operator === '' || currOperand === '') break;
+            compute();
             expression = '';
+            prevOperand = '';
+            currOperand = result;
+            expression = result;
+            result = '';
+            pressedSubmit = true;
             break;
         case 'add':
         case 'subtract':
         case 'multiply':
         case 'divide':
-            operator(value);
+            chooseOperation(value);
+            break;
+        case 'number':
+            if (pressedSubmit) clear(); 
+            expression += value;
+            currOperand += value;
             break;
     }    
-    updateDisplay();
-}
-
-function updateDisplay(){
+    // Update the display
     expressionDiv.textContent = expression;
     resultDiv.textContent = result;
+    console.log("prevOperand:",prevOperand,"Operator:",operator," currOperand:",currOperand)
 }
 
 function clear() {
     expression = '';
     result = '';
-}
-
-function negate() {
-    if (expression == '' || expression == 0){
-        return;
-    }
-    expression *= -1;
-}
-
-function percentage() {
-    expression = expression / 100;
+    prevOperand = '';
+    currOperand = '';
+    operator = '';
+    pressedSubmit = false;
 }
 
 function decimal() {
+    // If there is already a decimal, do nothing
+    if (expression.includes('.')) return; 
+    
+    // If the current expression is blank, append a leading '0'
+    if (expression === '') appendValue('0');
+
+    expression =+ '.'; // Ok now you can do the thing
     return;
-}
-
-function appendValue(value) {
-    expression += value;
-}
-
-function backspace() {
-    expression = expression.slice(0, -1);
 }
 
 function evaluateExpression() {
     result = expression;
 }
 
-function operator(value) {
-    if (expression ==='' && result !==''){
-        expression += result + value;
-    } else if (expression !== '' && !isNaN(parseInt(expression.slice(-1)))){
-        appendValue(value);
+function chooseOperation(value) {
+    if (operator != ''){
+        compute();
+        prevOperand = result;
+        operator ='';
+    } else {
+        prevOperand = currOperand;
     }
+    operator = value;
+    currOperand = '';
+    expression += value;
+    pressedSubmit=false;
 }
+
+function compute() {
+    let prev = parseFloat(prevOperand);
+    let curr = parseFloat(currOperand);
+    switch (operator){
+        case '+':
+            result = prev + curr;
+            break;
+        case '-':
+            result = prev - curr;
+            break;
+        case '*':
+            result = prev * curr;
+            break;
+        case '/':
+            result = prev / curr;
+            break;
+        default:
+            return;
+    }
+    operator = '';
+}
+
+
+    // appendValue(value);
+    // result = expression;
+    // expression = '';
+
+    // if (expression ==='' && result !==''){
+    //     expression += result + value;
+    // } else if (expression !== '' && !isNaN(parseInt(expression.slice(-1)))){
+    //     appendValue(value);
+    // }
